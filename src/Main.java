89a524661419e056b6a41a9d1d8631e9fcd55c95
janmertes <janmertes1913@gmail.com>
2026-01-19 19:57:10 +0100

    Initial BubbleSort

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main() {
+  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+  // to see how IntelliJ IDEA suggests fixing it.
+  int [] arr = {0, 6, 4, 9, 3, 7, 50, 890, 3};
+  BubbleSortExample.bubbleSort(arr);
+}
