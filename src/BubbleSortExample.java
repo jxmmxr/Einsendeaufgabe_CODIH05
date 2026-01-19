0e01b9ff3d01644d32cb4823ac1996742192967c
janmertes <janmertes1913@gmail.com>
2026-01-19 20:00:53 +0100

    Add early-exit optimization to BubbleSort

--- src/BubbleSortExample.java
+++ src/BubbleSortExample.java
@@ -2,15 +2,19 @@ public class BubbleSortExample {
     static void bubbleSort(int[] arr) {
         int n = arr.length;
         int temp = 0;
+        boolean changed;
         for(int i=0; i < n; i++){
+            changed = false;
             for(int j=1; j < (n-i); j++){
                 if(arr[j-1] > arr[j]){
                     //swap elements
                     temp = arr[j-1];
                     arr[j-1] = arr[j];
                     arr[j] = temp;
+                    changed = true;
                 }
             }
+            if (!changed) break;
         }
     }
 }
